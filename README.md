3616aef122d5ec137b85d341a9edf6f1f00622a3
“Elena <elemoser@gmail.com>
2024-03-14 16:48:56 +0100

    Add script for testing tools

--- README.md
+++ README.md
@@ -4,3 +4,27 @@ Web Accessibility Testing Site
 Describe our work and add thesis link here later.
 
 Description of how the website works and how to set it up.
+
+**Table of contents**
+- [Web Accessibility Testing Site](#web-accessibility-testing-site)
+    - [Running the app locally with docker](#running-the-app-locally-with-docker)
+    - [Testing the tools](#testing-the-tools)
+
+## Running the app locally with docker
+
+```
+# to run server in background
+docker compose up -d server
+
+# to shut down
+docker compose down
+```
+
+## Testing the tools
+
+In order to test the different tools, we created a bash-script (work in progress).
+
+```
+# to view intructions
+./test.bash
+```
---
+++ html_cs.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+
+// Replace with the url you wish to test.
+// const url = 'http://localhost:1338/';
+var url = process.argv[2];
+
+// Replace with the path to the chrome executable in your file system. This one assumes MacOSX.
+const executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath
+  });
+
+  const page = await browser.newPage();
+
+  page.on('console', msg => {
+    console.log(msg.text())
+  });
+
+  await page.goto(url);
+
+  await page.addScriptTag({
+    path: 'node_modules/html_codesniffer/build/HTMLCS.js'
+  });
+
+  await page.evaluate(function () {
+    HTMLCS_RUNNER.run('WCAG2AA');
+  });
+
+  await browser.close();
+})();

f73bf68b736dc6cd0995ca4c89e2c64009b31c17
“Elena <elemoser@gmail.com>
2024-03-26 16:19:05 +0100

    Fix issues with report

--- generate_report.bash
+++ generate_report.bash
@@ -4,8 +4,8 @@ OUTPUT_FILE="results/report.json"
 JS_FILE="generate_report.js"
 
 # Axe provides the categories: inapplicable, passes, incomplete, violation
-# Axes fail condition: violation (non customizable)
-# Axes reports: inapplicable, passes, incomplete, violation (non customizable)
+# Axe fail condition: violation (non customizable)
+# Axe reports: inapplicable, passes, incomplete, violation (non customizable)
 
 # Achecker provides the categories: "violation", "potentialviolation","recommendation","potentialrecommendation",
 # "manual","pass","ignored","elements","elementsViolation","elementsViolationReview"
@@ -22,24 +22,18 @@ function axe-results-summary {
 
     # Check if the target is not a directory
     if [ ! -d "$axe_directory" ]; then
-    echo "Not a directory."
+    echo "The '$axe_directory' directory does not exist."
     exit 1
     fi
 
     # Loop through files in the target directory
     for file in "$axe_directory"/*; do
     if [ -f "$file" ]; then
-        # url examined
-        url=$(jq '.[0].url' "$file")
-        # total errors for url
-        # total_errors=$(jq '[.[0].violations[].nodes | length | tonumber ] | add | if . == null then 0 else . end' "$file")
-        total_errors=$(jq '.[].violations | length' "$file")
-        echo "Axe run on $url found $total_errors error(s)."
-        node $JS_FILE "./$file"
+        node "$JS_FILE" "axe" "./$file"
     fi
     done    
     
-    echo "---------------------------------------------"
+    echo "Axe results report is done."
 }
 
 function achecker-results-summary {
@@ -48,25 +42,25 @@ function achecker-results-summary {
 
     # Check if the target is not a directory
     if [ ! -d "$achecker_directory" ]; then
-    echo "Not a directory."
+    echo "The '$achecker_directory' directory does not exist."
     exit 1
     fi
 
     # Rename home page file
     # Needed for being read in the loop below! 
-    mv "$achecker_directory/.json" "$achecker_directory/home.json"
+    if [ -e "$achecker_directory/.json" ]; then
+        mv "$achecker_directory/.json" "$achecker_directory/home.json"
+    fi
+    
 
     # Loop through files in the target directory
     for file in "$achecker_directory"/*; do
     if [ -f "$file" ]; then
-        url=$(jq '.summary.URL' "$file")
-        total_errors=$(jq '.summary.counts.violation' "$file")
-        echo "Achecker run on $url found $total_errors error(s)."
-        node $JS_FILE "./$file"
+        node "$JS_FILE" "achecker" "./$file"
     fi
     done
 
-    echo "---------------------------------------------"
+    echo "Accessibility-checker results report is done."
 }
 
 function htmlcs-results-summary {
@@ -75,26 +69,27 @@ function htmlcs-results-summary {
 
     # Check if the target is not a directory
     if [ ! -d "$htmlcs_directory" ]; then
-    echo "Not a directory."
+    echo "The '$htmlcs_directory' directory does not exist."
     exit 1
     fi
 
     # Loop through files in the target directory
     for file in "$htmlcs_directory"/*; do
     if [ -f "$file" ]; then
-        # url examined
-        url=$( head -n 1 "$file" )
-        # # total numbers of errors
-        total_errors=$( grep -wc "Error" "$file")
-        echo "HTML_CodeSniffer run on $url found $total_errors error(s)."
-        node $JS_FILE "./$file"
+        node "$JS_FILE" "htmlcs" "./$file"
     fi
     done
 
-    echo "---------------------------------------------"
+    echo "HTML_CodeSniffer results report is done."
+
 }
 
-echo '{"summary": [], "report": []}' > $OUTPUT_FILE
-achecker-results-summary
-axe-results-summary
-htmlcs-results-summary
+function main {
+    # Create output file to write to
+    echo '{"summary": [], "report": []}' > $OUTPUT_FILE
+    achecker-results-summary
+    axe-results-summary
+    htmlcs-results-summary
+}
+
+main
--- generate_report.js
+++ generate_report.js
@@ -2,19 +2,20 @@
 const fs = require("fs");
 
 // Check if a filename is provided as an argument
-if (process.argv.length < 3) {
-    console.error("Usage: node achecker_generate_report.js <filename>");
+if (process.argv.length < 4) {
+    console.error("Usage: node generate_report.js <tool> <filename>");
     process.exit(1); // Exit with error code 1
 }
 
 // Extract the filename from the command-line arguments
-const path = process.argv[2];
+const tool = process.argv[2];
+const path = process.argv[3];
 
 // Global variables
-const tool = path.split('/')[2];
 const outputFilePath = "results/report.json";
 const jsonData = require('./results/report.json');
 
+// Function to append to the content of the current jsonData 
 function updateReport(updatedData) {
     const jsonString = JSON.stringify(updatedData);
 
@@ -23,128 +24,133 @@ function updateReport(updatedData) {
       });
 }
 
+// Function to create a new violation object with default values
+function createViolationObject(toolName,pageUrl) {
+    return {
+        tool: toolName,
+        url: pageUrl,
+        error_name: "",
+        error_description: "",
+        error_position: ""
+    };
+}
+
 function generateAxeReport(filePath) {
     const results = require(filePath);
     const urlPath = results[0].url;
-    let violationCount = 0; // for summary
-
-    // Function to create a new violation object with default values
-    function createViolationObject() {
-        return {
-            tool: tool,
-            url: urlPath,
-            error_name: "",
-            error_description: "",
-            error_position: ""
-        };
+    let summary = {
+        tool: tool,
+        url: urlPath,
+        fails: 0
     }
-    
+
     if (results[0].violations.length > 0) {
         for (key in results[0].violations) {
-                // console.log(violationCount);
-                // console.log(results[0].violations[key].id);
-                // console.log(results[0].violations[key].description);
-                // violationCount += 1;
             results[0].violations[key].nodes.forEach((node) => {
-                // console.log(violationCount);
-                // console.log(node.any[0].id);
-                // console.log(node.any[0].message);
-                // console.log(node.html);
-                const violation = createViolationObject();
-                violation['error_name'] = node.any[0].id;
-                violation['error_description'] = node.any[0].message;
-                violation['error_position'] = node.html;
-                jsonData.report.push(violation);
-                violationCount += 1;
+                node.any.forEach((error) => {
+                    const violation = createViolationObject(tool, urlPath);
+
+                    violation['error_name'] = error.id;
+                    violation['error_description'] = error.message;
+                    violation['error_position'] = node.html;
+                    summary.fails += 1;
+                    jsonData.report.push(violation);
+                });
             });
         }
-    } else {
-        const violation = createViolationObject();
-        jsonData.report.push(violation);
-    }
+    } 
+    // else {
+    //     const violation = createViolationObject();
+    //     jsonData.report.push(violation);
+    // }
     
+    jsonData.summary.push(summary);
+    // console.log(jsonData);
     updateReport(jsonData);
 }
 
 function generateAcheckerReport(filePath) {
     const results = require(filePath);
+    const toolName = "accessibility-checker";
     const urlPath = results.summary.URL;
-    const violationCount = results.summary.counts.violation;
-    // let summary = {};
-    let violation = {};
+    const summary = {
+        tool: toolName,
+        url: urlPath,
+        fails: results.summary.counts.violation
+    };
 
-    violation['tool'] = tool;
-    violation['url'] = urlPath;
-    violation['error_name'] = "";
-    violation['error_description'] = "";
-    violation['error_position'] = "";
-
-    // Check if violations found
-    // console.log(violationCount);
-
-    if (violationCount > 0) {
+    if (summary.fails > 0) {
         // Extract violations
         for (key in results.results) {
             if (results.results[key].level == "violation") {
+                const violation = createViolationObject(toolName, urlPath);
+
                 violation['error_name'] = results.results[key].ruleId;
                 violation['error_description'] = results.results[key].message;
                 violation['error_position'] = results.results[key].snippet;
                 jsonData.report.push(violation);
             }
         }
-    } else {
-        jsonData.report.push(violation);
     }
+    // else {
+    //     const violation = createViolationObject(urlPath);
+    //     jsonData.report.push(violation);
+    // }
 
-    // console.log(jsonData.report);
+    // Update summary
+    jsonData.summary.push(summary);
+    // console.log(jsonData);
     updateReport(jsonData);
 }
 
 function generateHTMLCSReport(filePath) {
     // Read txt file and extract results details
     fs.readFile(filePath, (err, data) => {
+        // Exit in case of error
         if (err) {
             console.error("Error reading file:", err);
             return;
         }
 
-        const urlPath = data.toString().split('\n')[0];
-        let violationCount = 0;
-        let violation = {};
+        const lines = data.toString().split('\n');
+        const uniqueLines = new Set(lines);
+        const urlPath = lines[0];
+        const toolName = "HTML_CodeSniffer";
+        let summary = {
+            tool: toolName,
+            url: urlPath,
+            fails: 0
+        };
         let itemsArray = [];
 
-        violation['tool'] = tool;
-        violation['url'] = urlPath;
-        violation['error_name'] = "";
-        violation['error_description'] = "";
-        violation['error_position'] = "";
-
-        data.toString().split('\n').forEach(line => {
+        uniqueLines.forEach(line => {
             if (line.includes("Error")) {
-                violationCount++;
+                const violation = createViolationObject(toolName,urlPath);
+
                 itemsArray = line.split('|');
                 violation['error_name'] = itemsArray[1];
                 violation['error_description'] = itemsArray[4];
                 violation['error_position'] = itemsArray[5];
-                // Save results
+                summary.fails++;
                 jsonData.report.push(violation);
             }
         });
 
-        if (violationCount === 0) {
-            // Save results
-            jsonData.report.push(violation);
-        }
-
+        // if (violationCount === 0) {
+        //     // Save results
+        //     jsonData.report.push(violation);
+        // }
+        
+        // Update summary
+        jsonData.summary.push(summary);
+        // console.log(jsonData);
         updateReport(jsonData);
     });
 }
 
-// console.log(tool);
-
 if (tool == "achecker") {
     generateAcheckerReport(path);
-} else if (tool == "HTML_CodeSniffer") {
+} else if (tool == "htmlcs") {
     generateHTMLCSReport(path);
 } else if (tool === "axe") {
     generateAxeReport(path);

385478640e941f4ffcfe269bfe0db00ca9df03a0
nils pettersson <np222je@student.lnu.se>
2025-05-14 16:41:46 +0200

    testing pipeline

---
+++ .lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run start', // Adjust to your app's dev/start command
+      startServerReadyPattern: 'Compiled successfully', // Waits until the app is ready
+      url: ['http://localhost:1338'], // URL that is being tests 
+    },
+    upload: {
+      target: 'temporary-public-storage', // instead of making a server 
+    },
+  },
+};

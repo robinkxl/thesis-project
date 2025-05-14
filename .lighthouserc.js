35e296747631e2e3fa17c88eebe849d3d5637065
nils pettersson <np222je@student.lnu.se>
2025-05-14 17:16:22 +0200

    adjusting sandbox settings

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,7 +4,10 @@ module.exports = {
       startServerCommand: 'npm run start', // Adjust to your app's dev/start command
       startServerReadyPattern: 'listening on port 1338', // cohesive with app.js now
       url: ['http://localhost:1338'], // URL that is being tests 
-      numberOfRuns: 1  // or 3?
+      numberOfRuns: 1,  // or 3?
+      settings: {
+        chromeFlags: ['--no-sandbox'], // bypass the sandbox error
+      },
     },
     upload: {
       target: 'temporary-public-storage', // instead of making a server 

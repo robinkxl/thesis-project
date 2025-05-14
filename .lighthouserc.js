d7990b161f1cd81008cc24b78686acafe722d0bb
nils pettersson <np222je@student.lnu.se>
2025-05-14 16:55:33 +0200

    testing without docker

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: 'npm run start', // Adjust to your app's dev/start command
-      startServerReadyPattern: 'Compiled successfully', // Waits until the app is ready
+      startServerReadyPattern: 'Compiled successfully, port 1338', // Waits until the app is ready
       url: ['http://localhost:1338'], // URL that is being tests 
     },
     upload: {

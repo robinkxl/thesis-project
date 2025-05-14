599fef18061e4f7e85962526df3e268732f4a0c1
nils pettersson <np222je@student.lnu.se>
2025-05-14 17:02:15 +0200

    adding before_scripts

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,8 +2,9 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: 'npm run start', // Adjust to your app's dev/start command
-      startServerReadyPattern: 'Compiled successfully, port 1338', // Waits until the app is ready
+      startServerReadyPattern: 'listening on port 1338', // cohesive with app.js now
       url: ['http://localhost:1338'], // URL that is being tests 
+      numberOfRuns: 1  // or 3?
     },
     upload: {
       target: 'temporary-public-storage', // instead of making a server 

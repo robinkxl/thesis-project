77e49141f05e2989bfb24757aef954608cc6fa0b
Sofia Kelasidou <sk224ap@student.lnu.se>
2025-05-22 22:42:26 +0200

    fix lighthouse rerunning npm start multiple times

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -13,6 +13,7 @@ module.exports = {
         preset: "desktop", // if we take this away it will evaluate the website as mobile view, so we could also do multiple tests for some particular criteria.
         chromeFlags: ['--no-sandbox'], // bypass the sandbox error
       },
+      outputDir: './results/lighthouse',
     },
     upload: {
       target: 'temporary-public-storage', // instead of making a server 

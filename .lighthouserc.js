3f7f8964d66657dbe950a214b11f3b5868735a8b
Sofia Kelasidou <sk224ap@student.lnu.se>
2025-05-22 23:05:19 +0200

    lighthouse reports not being generated

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,19 +6,17 @@ module.exports = {
         'http://localhost:1338/operable',
         'http://localhost:1338/understandable',
       ],
-      startServerCommand: 'npm run start', // Adjust to your app's dev/start command
       startServerReadyPattern: 'listening on port 1338', // cohesive with app.js now
       numberOfRuns: 1,  // this will be 1 for each url.
       settings: {
         preset: "desktop", // if we take this away it will evaluate the website as mobile view, so we could also do multiple tests for some particular criteria.
         chromeFlags: ['--no-sandbox'], // bypass the sandbox error
       },
+      output: ['html'], // Tell Lighthouse to generate HTML reports
+      outputPath: './.lighthouseci', // Where to put them
     },
     upload: {
       target: 'temporary-public-storage', // instead of making a server 
-    },
-    report: {
-      outputPath: './results/lighthouse',
     },
   },
 };

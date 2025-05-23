862704f1c964f470952f9867e5c4a673f1766945
Sofia Kelasidou <sk224ap@student.lnu.se>
2025-05-23 02:28:43 +0200

    non relative path for lighthouse results

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -13,7 +13,6 @@ module.exports = {
         preset: "desktop", // if we take this away it will evaluate the website as mobile view, so we could also do multiple tests for some particular criteria.
         chromeFlags: ['--no-sandbox'], // bypass the sandbox error
       },
-      outputPath: './.lighthouseci', // Where to put them
     },
     upload: {
       target: 'temporary-public-storage', // instead of making a server 

92c02618a85691d9a7eb58fdb1e44ea899b0de9f
Sofia Kelasidou <sk224ap@student.lnu.se>
2025-05-23 03:24:33 +0200

    results/lighthouse

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -17,5 +17,9 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage', // instead of making a server 
     },
+    report: {
+      output: ['html'],               // Important: generates .report.html files
+      outputPath: 'results/lighthouse', // Match accessibility-checker's output style
+    },
   },
 };

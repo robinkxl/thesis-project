e1af18daa3b38bddc363ac0d873f327dcb3f68b8
Sofia Kelasidou <sk224ap@student.lnu.se>
2025-05-23 02:09:40 +0200

    add npm start back

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,6 +6,7 @@ module.exports = {
         'http://localhost:1338/operable',
         'http://localhost:1338/understandable',
       ],
+      startServerCommand: 'npm run start', // Adjust to your app's dev/start command
       startServerReadyPattern: 'listening on port 1338', // cohesive with app.js now
       numberOfRuns: 1,  // this will be 1 for each url.
       settings: {

a84016ccb5f2bc67c5000791da7ffca42b82e453
nils pettersson <np222je@student.lnu.se>
2025-05-24 16:07:14 +0200

    lighthouse dir in config?

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,19 +6,20 @@ module.exports = {
         'http://localhost:1338/operable',
         'http://localhost:1338/understandable',
       ],
-      startServerCommand: 'npm run start', // Adjust to your app's dev/start command
+      startServerCommand: 'npm run start',
       startServerReadyPattern: 'listening on port 1338', // cohesive with app.js now
       numberOfRuns: 1,  // this will be 1 for each url.
       settings: {
         preset: "desktop", // if we take this away it will evaluate the website as mobile view, so we could also do multiple tests for some particular criteria.
-        chromeFlags: ['--no-sandbox'], // bypass the sandbox error
+        chromeFlags: ['--no-sandbox'], // bypass the sandbox error, but this doesnt seem recommended.
       },
     },
     upload: {
       target: 'temporary-public-storage', // instead of making a server 
     },
     report: {
-      outputPath: 'results/Lighthouse', // Match accessibility-checker's output style
+      outputPath: 'results/Lighthouse', 
+      // is this why it wasnt working in yml before?
     },
   },
 };
